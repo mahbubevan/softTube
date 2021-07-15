af10f9effcc0938d3a6102ae62ddf55cece1f9b5
Evan <evan@Evans-MacBook-Air.local>
2021-07-15 20:18:02 +0600

    Advertise Create

--- resources/views/admin/advertiser/create.blade.php
+++ resources/views/admin/advertiser/create.blade.php
@@ -8,65 +8,53 @@
                 <h4 class="card-title"> @lang('Advertiser Information') </h4>
             </div>
             <div class="card-body">
+                <div class="row addedField">
+                                   
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn float-right bg-indigo-500 hover:bg-indigo-700 text-white addField">
+                    @lang('Add Field')
+                </button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header bg-indigo-500 text-white">
+                <h4 class="card-title"> @lang('Advertise Information') </h4>
+            </div>
+            <div class="card-body">
                 <div class="row">
                     <div class="form-group col-md-6">
-                        <label for="name"> @lang('Name') <span class="text-pink-700">*</span> </label>
-                        <input type="text" id="name" name="name" class="form-control" placeholder="@lang('Your Plan Name')">
+                        <label for="addName"> @lang('Ad Name') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="addName" name="price" class="form-control" placeholder="@lang('Ad Name')">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="price"> @lang('Price') <span class="text-pink-700">*</span> </label>
-                        <input type="text" id="price" name="price" class="form-control" placeholder="@lang('Price For Your Plan')">
+                        <label for="addSize"> @lang('Ad Size') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="addSize" name="add_size" class="form-control" placeholder="@lang('Ad Size')">
+                    </div>
+    
+                    <div class="form-group col-md-6">
+                        <label for="addType"> @lang('Ad Type') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="addType" name="add_type" class="form-control" placeholder="@lang('Ad Size')">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="storage"> @lang('Storage') <span class="text-pink-700">*</span> </label>
-                        <div class="input-group">
-                            <input type="number" id="storage" name="storage" class="form-control" placeholder="@lang('Storage For User')">
-                            <select name="storage_unit" required id="storage_unit" class="input-group-text">
-                                <option value=""> @lang('Select unit') </option>
-                                <option value="{{\App\Models\Plan::MB}}"> @lang('MB') </option>
-                                <option value="{{\App\Models\Plan::GB}}"> @lang('GB') </option>
-                                <option value="{{\App\Models\Plan::TB}}"> @lang('TB') </option>
-                            </select>
-                        </div>
+                        <label for="image"> @lang('Image') <span class="text-pink-700">*</span> </label>
+                        <input type="file" id="image" name="image" class="form-control" />
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="storage"> @lang('Storage Type') <span class="text-pink-700">*</span> </label>
-                        <select name="storage_type" required id="storage_type" class="form-control">
-                            <option value=""> @lang('Select Type') </option>
-                            <option value="{{\App\Models\Plan::LOCAL}}"> @lang('Local Disk') </option>
-                            <option value="{{\App\Models\Plan::AWS}}"> @lang('Amazon S3') </option>
-                        </select>
+                        <label for="script"> @lang('Script') <span class="text-pink-700">*</span> </label>
+                        <textarea id="script" name="script" class="form-control"></textarea>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="type"> @lang('Plan Purchase Type') <span class="text-pink-700">*</span> </label>
-                        <select name="type" required id="type" class="form-control">
-                            <option value=""> @lang('Select Type') </option>
-                            <option value="{{\App\Models\Plan::LIFETIME}}"> @lang('Once Payment For Lifetime') </option>
-                            <option value="{{\App\Models\Plan::RECURRING}}"> @lang('Recurring Payment') </option>
-                        </select>
+                        <label for="redirectUrl"> @lang('Redirect Url') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="redirectUrl" name="redirectUrl" class="form-control" />
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="renewal_type"> @lang('Plan Renewal Method') </label>
-                        <select disabled name="renewal_type" required id="renewal_type" class="form-control">
-                            <option value="{{\App\Models\Plan::MONTHLY}}"> @lang('Monthly') </option>
-                            <option value="{{\App\Models\Plan::YEARLY}}"> @lang('Yearly') </option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label for="withdrawal_type"> @lang('Withdrawal Method') <span class="text-pink-700">*</span> </label>
-                        <select name="withdrawal_type" required id="withdrawal_type" class="form-control">
-                            <option value=""> @lang('Select Method') </option>
-                            <option value="{{\App\Models\Plan::DAILY}}"> @lang('DAILY') </option>
-                            <option value="{{\App\Models\Plan::WEEKLY}}"> @lang('WEEKLY') </option>
-                            <option value="{{\App\Models\Plan::MONTHLY}}"> @lang('MONTHLY') </option>
-                            <option value="{{\App\Models\Plan::YEARLY}}"> @lang('YEARLY') </option>
-                        </select>
-                    </div>
+
                 </div>
             </div>
             <div class="card-footer">
                 <button type="submit" class="btn float-right bg-indigo-500 hover:bg-indigo-700 text-white">
-                    @lang('Add')
+                    @lang('Submit')
                 </button>
             </div>
         </div>
@@ -77,7 +65,20 @@
     <script>
         "use strict"
         $(function(){
-            
+            var i = 0;
+            $(".addField").on("click",function(){
+                i++
+                $(".addedField").append(`
+                    <div class="form-group col-md-3">
+                        <label for="key"> @lang('Key') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="key${i}" name="key[]" class="form-control" placeholder="@lang('Full Name')">
+                    </div>     
+                    <div class="form-group col-md-3">
+                        <label for="value"> @lang('Value') <span class="text-pink-700">*</span> </label>
+                        <input type="text" id="value${i}" name="value[]" class="form-control" placeholder="@lang('John Doe')">
+                    </div>     
+                `)
+            })
         
         })
     </script>
